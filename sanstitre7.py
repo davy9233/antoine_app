c3023fd83ddf290ab1290c828be270d07005ad63
davy9233 <boulot.davy@free.fr>
2020-01-10 16:55:01 +0100

    4 fois

--- sanstitre7.py
+++ sanstitre7.py
@@ -15,7 +15,8 @@ Lister les 10 derniers livres par leur date de publication
 import requests
 r = requests.get('https://demo.api-platform.com/books?order[publicationDate]=desc&page=1')
 data1=(r.json())
-s=0
+s=0 
+print('Lister les 10 derniers livres par leur date de publication')
 while s< 11:
     print(data1["hydra:member"][s]['title'])
     s+=1
@@ -29,8 +30,10 @@ Lister le livre écrit par l’auteur « Dr. Kaitlyn Ratke »
 import requests
 r2 = requests.get('https://demo.api-platform.com/books?author=Dr.%20Kaitlyn%20Ratke&page=1')
 data2=(r2.json())
+print("\n") 
+print("livre écrit par l’auteur « Dr. Kaitlyn Ratke »")
 for i in range(len(data2["hydra:member"])):
-    print("\n")
+   
     print(data2["hydra:member"][i]['title'])
 
    
@@ -44,6 +47,7 @@ import requests
 r3 = requests.get('https://demo.api-platform.com/books/1d52ba85-97c8-4cc3-b81a-40582f3aff64')
 data3=(r3.json())
 print("\n")
+print("les commentaires du livre dont l’id est « 1d52ba85-97c8-4cc3-b81a-40582f3aff64")
 for el in data3['reviews']:
     print(el["body"])
 
@@ -57,52 +61,34 @@ dont l’id est « 1b08c9ab-6254-4015-ad14-bac3e5c008df »
 """
 import requests
 data4={
-  "body": "test_dca",
+  "body": "test_dca_2",
   "rating": 5,
-  "author": "dca",
+  "author": "dca_2",
   "publicationDate": "2020-01-10T09:00:40.196Z",
   "book": "/books/1b08c9ab-6254-4015-ad14-bac3e5c008df"
 }
 r = requests.post('https://demo.api-platform.com/reviews', json=data4)
 
 
+
+#exo 5
+
+
 """
- Modifier votre nouveau commentaire en utilisant l’id qui vous a été fourni lors de sa
+Modifier votre nouveau commentaire en utilisant l’id qui vous a été fourni lors de sa
 création
-
 """
- 
-data={
-  "body": "test_dca 2eme avis",
-  "rating": 5,
-  "author": "dca",
-  "publicationDate": "2020-01-10T09:00:40.196Z",
-  "book": "/books/1b08c9ab-6254-4015-ad14-bac3e5c008df"
-}
 
+import requests
 
-url ='https://demo.api-platform.com/books/1b08c9ab-6254-4015-ad14-bac3e5c008df/reviews?page=1'
-r= requests.get(url)
-liste4=r.json()
-for i in range(len(liste4['hydra:member'])):
-    print(liste4['hydra:member'][i]['author'])
+url = "https://demo.api-platform.com/reviews/00421975-a3c2-44ab-88af-99ceda201e91"
 
+payload = "{\n\t\"body\" : \"trop dur\"\n}"
+headers = {
+    'cookie': "__cfduid=dd0757c4290287936d03f1028b00579661578569236",
+    'content-type': "application/merge-patch+json"
+    }
 
-s1 = requests.session()
-r1 = s1.patch('https://demo.api-platform.com/reviews', json=data)
+response = requests.request("PATCH", url, data=payload, headers=headers)
 
-# 6
-s = '/books/1b08c9ab-6254-4015-ad14-bac3e5c008df'
-a ='dca'
-#s='1d52ba85-97c8-4cc3-b81a-40582f3aff64'    
-r6 = requests.get('https://demo.api-platform.com/reviews')
-data6=(r6.json())
-result=[]
-print("\n")
-for i in range(len(data6["hydra:member"])):
-    lost=data6["hydra:member"][i]
-    if s in (lost['book']['@id']): 
-                if a == str(lost["author"]):
-                    result.append(lost["@id"])
-print (result)
-""" 
+print(response.text)
